fb252b5df39399fd25da174cbc8649a0e8a5a9d3
HiteshAttri <hiteshattri71@gmail.com>
2023-06-26 11:36:20 +0530

    checkpoint 26 june

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const chats = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 dotenv.config();
 
 connectDB();
 
+app.use(express.json());
+
 const cors = require("cors");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 app.use(
   cors({
@@ -20,15 +25,20 @@ app.get("/", (req, res) => {
   res.send("get request");
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chats/:id", (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.json(singleChat);
-});
+// app.get("/api/chats", (req, res) => {
+//   res.send(chats);
+// });
+
+// app.get("/api/chats/:id", (req, res) => {
+//   // console.log(req.params.id);
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.json(singleChat);
+// });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
